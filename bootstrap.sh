93bac66a4924e7491bbc9647ef038077c97b798a
Nitin Kalra <kalni03@ca.com>
2016-07-07 13:17:42 -0700

    Removing maven dependency

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,8 @@
 
 #oc new-project stage
 oc new-project ci
+#oc create serviceaccount jenkins -n ci
+#oadm policy add-scc-to-user privileged system:serviceaccount:ci:jenkins
 oc policy add-role-to-user edit system:serviceaccount:ci:default -n ci
 #oc policy add-role-to-user edit system:serviceaccount:stage:default -n ci
 #oc policy add-role-to-user edit system:serviceaccount:ci:default -n stage
